671daeac62c3e20269e72cc630c8886d03729899
EAguirre <aguirreernesto25@gmail.com>
2023-10-30 20:27:59 -0300

    Actualizando

--- src/App.vue
+++ src/App.vue
@@ -1,106 +1,94 @@
 <script setup>
-// Me quede en el video 57
+// Este es el componente padre
+import { ref, reactive, onMounted, watch } from "vue"
+import { db } from "./data/guitarras"
 
-    // Este es el componente padre
-    import { ref, reactive, onMounted, watch } from "vue"
-    import { db } from "./data/guitarras"
+import Guitarra from "./components/Guitarra.vue" // Componente donde aparecen todas las guitarras
+import Header from "./components/Header.vue" //  Componente donde esta el carrito
+import Footer from "./components/Footer.vue" // Componente Footer
 
-    import Guitarra from "./components/Guitarra.vue" // Componente donde aparecen todas las guitarras
-    import Header from "./components/Header.vue" //  Componente donde esta el carrito
-    import Footer from "./components/Footer.vue" // Componente Footer
+// const state = reactive ({
+//     guitarras: db
+// })
 
-    // const state = reactive ({
-    //     guitarras: db
-    // })
+// STATES
+const guitarras = ref([])  // State de Guitarras (aquí se encuentran todas las guitarras)
+const carrito = ref([])    // State de Carrito de Compras (aquí se agregan las guitarras)
+const guitarra = ref({})   // State de una Guitarra
 
-    // STATES
-    const guitarras = ref([])  // State de Guitarras (aquí se encuentran todas las guitarras)
-    const carrito = ref([])    // State de Carrito de Compras (aquí se agregan las guitarras)
-    const guitarra = ref({})   // State de una Guitarra
-    
-    // Función agregarCarrito para pasar como un event
-    const agregarCarrito = (guitarra) => {
-        const exiteEnCarrito = carrito.value.findIndex(producto => producto.id === guitarra.id)
-        
-        if(exiteEnCarrito >= 0) {
-            carrito.value[exiteEnCarrito].cantidad++
-        } else {
-            // La cantidad con la que se inicia
-            guitarra.cantidad = 1;
+// Función agregarCarrito para pasar como un event
+const agregarCarrito = (guitarra) => {
+    const exiteEnCarrito = carrito.value.findIndex(producto => producto.id === guitarra.id)
 
-            // Se agrega al state de carrito (un arreglo) la guitarra
-            carrito.value.push(guitarra);
-        }
-    };
-    
-    watch(carrito, () => {
-        guardarCarrito()
-    }, {
-        deep: true
-    })
+    if (exiteEnCarrito >= 0) {
+        carrito.value[exiteEnCarrito].cantidad++
+    } else {
+        // La cantidad con la que se inicia
+        guitarra.cantidad = 1;
 
-    onMounted(() => {
-        guitarras.value = db;
-        guitarra.value = db[3]
-        
-        const carritoStorage = localStorage.getItem('carrito')
-        if(carritoStorage){
-            carrito.value = JSON.parse(carritoStorage)
-        }
-    })
+        // Se agrega al state de carrito (un arreglo) la guitarra
+        carrito.value.push(guitarra);
+    }
+};
 
-    const guardarCarrito = () => {
-        localStorage.setItem("carrito", JSON.stringify(carrito.value))
-    };
+watch(carrito, () => {
+    guardarCarrito()
+}, {
+    deep: true
+})
 
-    const decrementarCantidad = (id) => {
-        const index = carrito.value.findIndex(producto => producto.id === id)
-        if(carrito.value[index].cantidad <= 1) return
-        carrito.value[index].cantidad--
-    };
+onMounted(() => {
+    guitarras.value = db;
+    guitarra.value = db[3]
 
-    const incrementarCantidad = (id) => {
-        const index = carrito.value.findIndex(producto => producto.id === id)
-        if(carrito.value[index].cantidad >= 5) return
-        carrito.value[index].cantidad++
-    };
+    const carritoStorage = localStorage.getItem('carrito')
+    if (carritoStorage) {
+        carrito.value = JSON.parse(carritoStorage)
+    }
+})
 
-    const eliminarProducto = (id) => {
-        carrito.value = carrito.value.filter(producto => producto.id !== id)
-    };
+const guardarCarrito = () => {
+    localStorage.setItem("carrito", JSON.stringify(carrito.value))
+};
 
-    const vaciarCarrito = () => {
-        carrito.value = []
-    };
+const decrementarCantidad = (id) => {
+    const index = carrito.value.findIndex(producto => producto.id === id)
+    if (carrito.value[index].cantidad <= 1) return
+    carrito.value[index].cantidad--
+};
+
+const incrementarCantidad = (id) => {
+    const index = carrito.value.findIndex(producto => producto.id === id)
+    if (carrito.value[index].cantidad >= 5) return
+    carrito.value[index].cantidad++
+};
+
+const eliminarProducto = (id) => {
+    carrito.value = carrito.value.filter(producto => producto.id !== id)
+};
+
+const vaciarCarrito = () => {
+    carrito.value = []
+};
 </script>
 
 <template>
-    <Header
-        :carrito="carrito"
-        :guitarra="guitarra"
-        @decrementar-cantidad="decrementarCantidad"
-        @incrementar-cantidad="incrementarCantidad"
-        @agregar-carrito="agregarCarrito"
-        @eliminar-Producto="eliminarProducto"
-        @vaciar-carrito="vaciarCarrito"
-    />
+    <Header :carrito="carrito" :guitarra="guitarra" @decrementar-cantidad="decrementarCantidad"
+        @incrementar-cantidad="incrementarCantidad" @agregar-carrito="agregarCarrito" @eliminar-Producto="eliminarProducto"
+        @vaciar-carrito="vaciarCarrito" />
 
-        <main class="container-xl mt-5">
-            <h2 class="text-center">Nuestra Colección</h2>
+    <main class="container-xl mt-5">
+        <h2 class="text-center">Nuestra Colección</h2>
 
-            <div class="row mt-5">
-                <!-- Se renderiza el componente -->
+        <div class="row mt-5">
+            <!-- Se renderiza el componente -->
 
-                <!-- Se hace la iteración del componente con v-for -->
-                <!-- Props guitarra para que vaya al componente Guitarra.vue -->
-                <!-- Event para agregar carrito  -->
-                <Guitarra
-                    v-for="guitarra in guitarras" 
-                    :guitarra="guitarra"
-                    @agregar-carrito="agregarCarrito"
-                    :key="guitarra.id"
-                />
-            </div>
-        </main>
-    <Footer/>
+            <!-- Se hace la iteración del componente con v-for -->
+            <!-- Props guitarra para que vaya al componente Guitarra.vue -->
+            <!-- Event para agregar carrito  -->
+            <Guitarra v-for="guitarra in guitarras" :guitarra="guitarra" @agregar-carrito="agregarCarrito"
+                :key="guitarra.id" />
+        </div>
+    </main>
+    <Footer />
 </template>
--- src/components/Guitarra.vue
+++ src/components/Guitarra.vue
@@ -1,14 +1,14 @@
 <script setup>
-    // Este es el componente hijo
-    // Props que viene desde App.vue
-    const props = defineProps({
-        guitarra: {
-            type: Object,   // Tipo de dato que se pasa
-            required: true  // Si es requerido
-        }
-      }) 
+// Este es el componente hijo
+// Props que viene desde App.vue
+const props = defineProps({
+    guitarra: {
+        type: Object,   // Tipo de dato que se pasa
+        required: true  // Si es requerido
+    }
+})
 
-    defineEmits(['agregar-carrito'])
+defineEmits(['agregar-carrito'])
 </script>
 
 <template>
@@ -16,34 +16,25 @@
         <div class="col-4">
 
             <!-- Atributos dinámicos -->
-            <img 
-            class="img-fluid" 
-            :src= "'/img/' + guitarra.imagen + '.jpg'" 
-            :alt="'imagen guitarra' + guitarra.nombre"
-            >
+            <img class="img-fluid" :src="'/img/' + guitarra.imagen + '.jpg'" :alt="'imagen guitarra' + guitarra.nombre">
 
         </div>
 
         <div class="col-8">
             <!-- Se muestra el nombre de cada guitarra-->
             <h3 class="text-black fs-4 fw-bold text-uppercase"> {{ guitarra.nombre }} </h3>
-            
+
             <!-- Se muestra la descripción de cada guitarra-->
             <p> {{ guitarra.descripcion }} </p>
-            
+
             <!-- Se muestra el precio de cada guitarra -->
             <p class="fw-black text-primary fs-3">${{ guitarra.precio }}</p>
-            
-            <button 
-                type="button"
-                class="btn btn-dark w-100 "
-                @click="$emit ('agregar-carrito', guitarra)"
-            >
+
+            <button type="button" class="btn btn-dark w-100 " @click="$emit('agregar-carrito', guitarra)">
                 Agregar al Carrito
             </button>
         </div>
     </div>
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
